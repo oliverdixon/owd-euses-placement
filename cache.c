cdc0ec74559d55ed208de24908da80219ecc72a7
Oliver Dixon <ash@suugaku.co.uk>
2020-06-15 14:25:18 +0100

    Defined the goals for the caching of repositories.

---
+++ cache.c
@@ -0,0 +1,12 @@
+/* ash-euses: repository-description caching
+ * Ashley Dixon. */
+
+/* TODO: implement:
+ *      - load_cache: attempt to load the cache into the repository stack;
+ *      - write_cache: if loaded with an invalidated cache, write the newfound
+ *        repository-description information to the cache.
+ *      - validate_cache: if the cache is more than a few days old, invalidate
+ *        it and write out a new cache.
+ *      - Various options in the argument-processor, allowing fine control of
+ *        the cache. */
+
